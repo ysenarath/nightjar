dedb14a06116cc381107778c81b22282a016421e
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-01-16 00:59:43 -0500

    Add debug print statement for attributes in AttributeMapMeta class

--- src/nightjar/base.py
+++ src/nightjar/base.py
@@ -35,6 +35,7 @@ class AttributeMapMeta(abc.ABCMeta):
         dispatch = kwargs.pop("dispatch", None)
         obj = super().__new__(mcls, name, bases, namespace)
         obj = dataclass(**kwargs)(obj)
+        # print all attributes
         if hasattr(obj, "_dispatch_registry"):
             obj._dispatch_registry.register(obj)
         elif dispatch is not None:
