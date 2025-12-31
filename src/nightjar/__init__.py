847e1f0a44efafc3a72374c37a980c8be5c2f936
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-31 16:52:19 -0500

    Bump version to 0.0.6 and remove unused classes from __init__.py

--- src/nightjar/__init__.py
+++ src/nightjar/__init__.py
@@ -1,21 +1,9 @@
 from nightjar.base import AutoModule, BaseConfig, BaseModule
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 __all__ = [
     "AutoModule",
     "BaseConfig",
     "BaseModule",
-    # fancy names for above classes in case you want to use them
-    "Jar",
-    "JarConfig",
 ]
-
-
-class JarConfig(BaseConfig): ...
-
-
-class Jar(BaseModule): ...
-
-
-class AutoJar(AutoModule): ...
